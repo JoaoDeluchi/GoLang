e7c790d406464e28b6098ece6058b640c4cc2238
joao.deluchi <brhbs0907@ambev.com.br>
2020-11-14 02:03:05 -0300

    Study API with GO

---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+	"github.com/gin-gonic/gin" 
+)
+
+func GetProduct(c *gin.Context){
+	c.JSON(200, gin.H{
+		"message": "Get Run well",
+	})
+}
+
+func CreateProduct(c *gin.Context){
+	c.JSON(200, gin.H{
+		"message": "Post Run ok",
+	})
+}
+
+func UpdateProduct(c *gin.Context){
+	c.JSON(200, gin.H{
+		"message": "Put ok"
+	})
+}
+
+func DeleteProduct(c *gin.Context){
+	c.JSON(200, gin.H{
+		"message": "Delete OK !!!"
+	})
+}
+func main() {
+	r := gin.Default()
+	r.GET("/product", GetProduct)
+	r.POST("/product", CreateProduct)
+	r.PUT("/product", UpdateProduct)
+	r.DELETE("/product", DeleteProduct)
+	r.Run()
+}
